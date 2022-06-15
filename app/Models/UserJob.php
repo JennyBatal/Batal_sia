14cc140ad724666115b6ad9224f568b28706c290
chrisjaneyamba <80240145+chrisjaneyamba@users.noreply.github.com>
2022-06-15 10:59:15 +0800

    Site2 Latest Code

--- app/Models/UserJob.php
+++ app/Models/UserJob.php
@@ -1,12 +1,13 @@
 <?php
  namespace App\Models;
  use Illuminate\Database\Eloquent\Model;
- class User extends Model{
- protected $table = 'tbluser';
+ class Userjob extends Model{
+ protected $table = 'tbluserjob';
  // column sa table
  protected $fillable = [
  'username', 'password', 'gender', 'jobid'
  ];
  public $timestamps = false;
+ 
  
  }
